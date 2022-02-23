b4bf498d1745cb70c1ce59432d51a2fa0131285d
Joel Okoromi <okmarq@gmail.com>
2022-02-23 15:53:56 +0100

    user table updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            // $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('phone_number')->unique();
             $table->string('password');
