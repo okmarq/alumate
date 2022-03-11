9a596f4265fcfe4d9ec1caf5aa01cf1c4b0c037d
Joel Okoromi <okmarq@gmail.com>
2022-03-11 11:25:16 +0100

    models update

--- app/Models/Alumni.php
+++ app/Models/Alumni.php
@@ -8,4 +8,45 @@ use Illuminate\Database\Eloquent\Model;
 class Alumni extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'graduation_year'
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function schools()
+    {
+        return $this->hasMany(School::class, 'school_id');
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'user_id');
+    }
 }
