c6637b354d2bf6d646cebbdce6354043ec74a27d
Joel Okoromi <okmarq@gmail.com>
2022-03-28 14:08:48 +0100

    user code updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
+// use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
