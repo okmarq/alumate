b62612d6f96eb4cd2b3f3bc6d70dc64a222d135e
Joel Okoromi <okmarq@gmail.com>
2022-03-28 13:10:03 +0100

    user factory updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'first_name' => $this->faker->firstName(),
             'last_name' => $this->faker->lastName(),
             'username' => $this->faker->userName(),
-            'invite_code' => $this->faker->random(8),
+            'invite_code' => Str::random(8),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
