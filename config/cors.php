26377c634671f420557e468ed4ef4221486313fb
Joel Okoromi <okmarq@gmail.com>
2022-03-29 16:18:47 +0100

    cors update

--- config/cors.php
+++ config/cors.php
@@ -15,15 +15,15 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api/admin/*', 'api/users/*', '*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://alumates111.herokuapp.com', '*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['Google', '*'],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['X-Custom-Header', 'Upgrade-Insecure-Requests', '*'],
 
     'exposed_headers' => [],
 
