4cd59180142dd5fdcd2461d03014b1dd2fd30c6c
Joel Okoromi <okmarq@gmail.com>
2022-03-30 02:37:48 +0100

    alumni seeder updated

--- database/seeders/AlumniSeeder.php
+++ database/seeders/AlumniSeeder.php
@@ -17,14 +17,12 @@ class AlumniSeeder extends Seeder
     {
         for ($i = 1; $i <= 20; $i++) {
             for ($j = 1; $j <= 20; $j++) {
-                for ($k = 1; $k <= 20; $k++) {
-                    DB::table('alumnis')->insert([
-                        'graduation_year' => 2000 + $k,
-                        'admission_year' => 1995 + $k,
-                        'school_id' => $j,
-                        'user_id' => $j
-                    ]);
-                }
+                DB::table('alumnis')->insert([
+                    'graduation_year' => 2000 + $i,
+                    'admission_year' => 1995 + $i,
+                    'school_id' => $i,
+                    'user_id' => $j
+                ]);
             }
         }
     }
