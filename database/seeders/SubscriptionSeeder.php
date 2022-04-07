d8b803a0b53fbb8cd44cef2286a3111d269ab4ea
Joel Okoromi <okmarq@gmail.com>
2022-04-07 16:56:31 +0100

    subscription seeder updated

--- database/seeders/SubscriptionSeeder.php
+++ database/seeders/SubscriptionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubscriptionSeeder extends Seeder
 {
@@ -14,6 +15,45 @@ class SubscriptionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('subscriptions')->insert([
+            'name' => 'bronze',
+            'description' => '',
+            'cost' => 0
+        ]);
+        DB::table('subscriptions')->insert([
+            'name' => 'silver',
+            'description' => '',
+            'cost' => 0
+        ]);
+        DB::table('subscriptions')->insert([
+            'name' => 'gold',
+            'description' => '',
+            'cost' => 0
+        ]);
+        DB::table('subscriptions')->insert([
+            'name' => 'platinum',
+            'description' => '',
+            'cost' => 0
+        ]);
+        DB::table('subscriptions')->insert([
+            'name' => 'free',
+            'description' => '',
+            'cost' => 0
+        ]);
+        DB::table('subscriptions')->insert([
+            'name' => 'basic',
+            'description' => '',
+            'cost' => 0
+        ]);
+        DB::table('subscriptions')->insert([
+            'name' => 'plus',
+            'description' => '',
+            'cost' => 0
+        ]);
+        DB::table('subscriptions')->insert([
+            'name' => 'pro',
+            'description' => '',
+            'cost' => 0
+        ]);
     }
 }
