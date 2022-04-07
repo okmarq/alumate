0371b9e2e631c0e5e3645084998136f1c87b3f23
Joel Okoromi <okmarq@gmail.com>
2022-04-07 16:56:16 +0100

    user migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,9 +23,15 @@ return new class extends Migration
             $table->string('invite_code')->unique();
             $table->string('invite_id')->nullable();
             // $table->string('phone_number')->unique();
+            // $table->string('gender');
+            // $table->string('phone_number_id')->nullable()->constrained()->onUpdate('restrict')->onDelete('restrict');
+            // $table->year('birth_year');
+            // $table->month('birth_month');
+            // $table->day('birth_day');
             $table->string('password');
             $table->tinyInteger('is_active')->default(0);
             $table->tinyInteger('is_verified')->default(0);
+            $table->tinyInteger('is_online')->default(0);
             $table->rememberToken();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
