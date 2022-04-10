97056facb962ccb361949e1613e09536f770fc96
Joel Okoromi <okmarq@gmail.com>
2022-04-10 12:52:50 +0100

    phone facades created

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -9,5 +9,24 @@ class Profession extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = ['name', 'required_skill_level', 'description'];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
