028939bb7864abba12597fc28d504924663ae6a2
Joel Okoromi <okmarq@gmail.com>
2022-04-14 04:04:47 +0100

    seeder updated

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -16,24 +16,52 @@ class DepartmentSeeder extends Seeder
     public function run()
     {
         DB::table('departments')->insert([
-            'name' => 'Accounting',
-        //     'school_id' => 1,
-        //     'department_id' => 1
+            'name' => 'Accounting'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Agriculture'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Arts'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Biological Sciences'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Administration'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Dentistry'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Education'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Engineering'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Environmental Sciences'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Health Sciences & Technology'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Law'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Medical Sciences'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Pharmaceutical Sciences'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Physical Sciences'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Social Sciences'
+        ]);
+        DB::table('departments')->insert([
+            'name' => 'Veterinary Medicine'
         ]);
     }
 }
-// Agriculture
-// Arts
-// Biological Sciences
-// Administration
-// Dentistry
-// Education
-// Engineering
-// Environmental Sciences
-// Health Sciences & Technology
-// Law
-// Medical Sciences
-// Pharmaceutical Sciences
-// Physical Sciences
-// Social Sciences
-// Veterinary Medicine
