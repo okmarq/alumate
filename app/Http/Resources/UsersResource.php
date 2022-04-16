298f17a12691da822e8452a1e88ffb767b0008e3
Joel Okoromi <okmarq@gmail.com>
2022-04-17 00:29:52 +0100

    users updated

--- app/Http/Resources/UsersResource.php
+++ app/Http/Resources/UsersResource.php
@@ -14,6 +14,16 @@ class UsersResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'username' => $this->username,
+            'email' => $this->email,
+            'invite_code' => $this->invite_code,
+            'invite_id' => $this->invite_id,
+            'status' => $this->status,
+        ];
     }
 }
