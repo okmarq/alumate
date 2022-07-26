f48851bec246d460c97f5483e41d9e8f86d577b4
Joel Okoromi <okmarq@gmail.com>
2022-07-26 03:31:05 +0100

    database updated

--- database/migrations/2020_04_28_000000_create_states_table.php
+++ database/migrations/2020_04_28_000000_create_states_table.php
@@ -14,18 +14,18 @@ class CreateStatesTable extends Migration
     public function up()
     {
         Schema::create('states', function (Blueprint $table) {
-            // $table->increments('id');
-            // $table->string('name')->index();
-            // $table->Integer('country_id')->unsigned()->nullable();
-            // $table->foreign('country_id')->references('id')->on('countries');
             $table->id();
             $table->string('name');
             $table->string('capital');
-            $table->foreignId('country_id')
-                ->constrained()
-                ->onUpdate('restrict')
-                ->onDelete('restrict');
-            $table->unique(['name', 'country_id']);
+            $table->string('country_id');
+            // $table->unsignedBigInteger('country_id')->nullable();
+            // $table->foreign('country_id')->references('id')->on('countries');
+            // $table->string('name');
+            // $table->foreignId('country_id')
+            //     ->constrained()
+            //     ->onUpdate('restrict')
+            //     ->onDelete('restrict');
+            // $table->unique(['name', 'country_id']);
         });
     }
 
